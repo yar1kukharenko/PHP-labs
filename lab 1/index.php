6ea4f18c6bc47780be645ae5a5d5f6b358ed1016
yar1kukharenko <113698528+yar1kukharenko@users.noreply.github.com>
2023-02-20 19:36:29 +0300

    1 лаба

---
+++ lab 1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo '<header><img src = "./mospolytech-logo-black.svg" alt = ""></header>';?>
+    <?php echo '<main><p style ="text-align: center ">Привет, мир!</p></main>';?>
+    <?php echo '<footer><p style ="text-align: center ">Создать веб-страницу с динамическим контентом. Загрузить код в удаленный репозиторий. Залить на хостинг.</p></footer>';?>
+</body>
+</html>
